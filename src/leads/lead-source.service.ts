bc816f21cf7b3bf0e16f90c5cde7f190e588c4d8
Meet Rakholiya <meet@investifyd.com>
2025-09-11 16:59:02 +0530

    add update and delete end points to leads source

--- src/leads/lead-source.service.ts
+++ src/leads/lead-source.service.ts
@@ -1,7 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { LeadSourceEntity } from '../db/entities/lead-source.entity';
 import { LeadSourceRepositoryInterface } from '../db/interfaces/lead-source.interface';
-import { CreateLeadSourceDto } from './dto/create-lead-source.dto';
+import {
+  CreateLeadSourceDto,
+  UpdateLeadSourceDto,
+} from './dto/create-lead-source.dto';
 
 @Injectable()
 export class LeadSourceService {
@@ -25,6 +28,38 @@ export class LeadSourceService {
   ): Promise<LeadSourceEntity> {
     try {
       return this.leadSourceRepository.save(createLeadSourceDto);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findById(id: string) {
+    try {
+      const source = await this.leadSourceRepository.findOneById(id);
+      if (!source) {
+        throw new NotFoundException('Lead source not found');
+      }
+      return source;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateLeadSource(id: string, updateLeadSourceDto: UpdateLeadSourceDto) {
+    try {
+      const source = await this.findById(id);
+      Object.assign(source, updateLeadSourceDto);
+      return this.leadSourceRepository.save(source);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteLeadSource(id: string) {
+    try {
+      const source = await this.findById(id);
+      await this.leadSourceRepository.remove(source);
+      return true;
     } catch (error) {
       throw error;
     }
